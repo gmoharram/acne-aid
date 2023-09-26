916d7fa483226a64788b93dcd436d283c911fd06
Gana <gmoharram@gmail.com>
2023-09-26 22:42:32 +0200

    Added filtering query

--- database/querying.py
+++ database/querying.py
@@ -19,6 +19,17 @@ async def get_record(record_id, data_model, session):
         )
 
 
+async def get_records_by_field(field_value, field_name, data_model, session):
+    statement = select(data_model).where(getattr(data_model, field_name) == field_value)
+    records = session.exec(statement).all()
+    return records
+
+
+async def select_all(data_model, session):
+    records = session.exec(select(data_model)).all()
+    return records
+
+
 async def update_record(record_id, data_model, updated_record, session):
     record = get_record(record_id, data_model, session=session)
     record_data = updated_record.dict(exclude_unset=True)
@@ -34,8 +45,3 @@ async def delete_record(record_id, data_model, session):
     record = get_record(record_id, data_model, session=session)
     session.delete(record)
     session.commit()
-
-
-async def select_all(data_model, session):
-    records = session.exec(select(data_model)).all()
-    return records
