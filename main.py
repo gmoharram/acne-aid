227a3473bf94e9e705c4eed003441ffe748dda56
Gana <gmoharram@gmail.com>
2023-09-27 11:01:58 +0200

    Configure Cross-Origin Resource Sharing (CORS)

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from routes.user import user_router
 from database.connection import conn
 
 app = FastAPI()
+
+# Register origins (allowed to access API)
+
+origins = ["*"]  # TODO: Restrict origins to frontend app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Define routes
 
 
 @app.get("/")
