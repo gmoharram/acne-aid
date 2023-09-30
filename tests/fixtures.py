2d9ae9be46a80d54b09e28f1490a83bad1a8f6eb
Gana <gmoharram@gmail.com>
2023-09-30 21:24:02 +0200

    Setup pytest testing

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -3,9 +3,19 @@ import httpx
 import pytest
 
 from main import app
-from models.user import User
 
 
+@pytest.fixture(scope="session")
+def event_loop():
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
+
+
+# Mock API #
 @pytest.fixture(scope="session")
 async def default_client():
     async with httpx.AsyncClient(app=app, base_url="http://app") as client:
--- tests/test_user_router.py
+++ tests/test_user_router.py
@@ -2,13 +2,22 @@ from fastapi import status
 import httpx
 import pytest
 
+from tests.fixtures import *
+
 
 @pytest.mark.asyncio
-async def test_sign_up(default_client: httpx.AsyncClient) -> None:
-    payload = {"email": "testuser@mail.com", "password": "testpassword"}
+async def test_sign_up(mocker, default_client: httpx.AsyncClient) -> None:
+    mocker.patch("routes.user.insert_record", return_value=None)
+    payload = {
+        "email": "testuser@mail.com",
+        "password": "testpassword",
+        "username": "test-user",
+        "sex": "female",
+        "birthdate": "2000-01-07",
+    }
     headers = {"accept": "application/json", "Content-Type": "application/json"}
-    expected_response = {"message": "User successfully registered!"}
+    expected_response = {"data": None, "message": "User successfully registered!"}
 
     response = await default_client.post("/user/signup", json=payload, headers=headers)
-    assert response.status_code == status.HTTP_200_OK
+    assert response.status_code == status.HTTP_201_CREATED
     assert response.json() == expected_response
