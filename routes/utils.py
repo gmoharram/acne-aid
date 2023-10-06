8e126a05c04e249847346bc84659ce62d645faf3
Gana <gmoharram@gmail.com>
2023-10-06 10:41:45 +0200

    complete "/experiment/get" route

--- routes/utils.py
+++ routes/utils.py
@@ -1,5 +1,5 @@
 from datetime import datetime, time
-from typing import List
+from typing import List, Tuple
 
 from models.experiment import (
     Experiment,
@@ -13,11 +13,18 @@ from models.experiment import (
 import pdb
 
 
-timing_mapping = {
+timing_to_hour_mapping = {
     Timing.MORNING: 8,
     Timing.NOON: 12,
     Timing.AFTERNOON: 16,
     Timing.NIGHT: 20,
+}
+
+hour_to_timing_mapping = {
+    8: Timing.MORNING,
+    12: Timing.NOON,
+    16: Timing.AFTERNOON,
+    20: Timing.NIGHT,
 }
 
 
@@ -26,9 +33,15 @@ def timing_and_order_to_timestamp(timing: Timing, order: int) -> time:
     The timestamp is currently only used for clustering and ordering the routine steps
     i.e. 8am -> morning, 12pm -> noon, 4pm -> afternoon, 8pm -> night and nth second -> nth step
     """
-    time_str = f"{timing_mapping[timing]}:0:{order}"
+    time_str = f"{timing_to_hour_mapping[timing]}:0:{order}"
     timestamp = datetime.strptime(time_str, "%H:%M:%S").time()
     return timestamp
+
+
+def timestamp_to_timing_and_order(timestamp: time) -> Tuple[Timing, int]:
+    timing = hour_to_timing_mapping[timestamp.hour]
+    order = timestamp.second
+    return timing, order
 
 
 def set_experiment_end_date(experiment_record: Experiment) -> Experiment:
@@ -37,13 +50,25 @@ def set_experiment_end_date(experiment_record: Experiment) -> Experiment:
 
 
 def joined_records_to_experiment_data(joined_records: List) -> ExperimentData:
+    experiment_record = joined_records[0][0]
+
     experiment_data = {}
-    pdb.set_trace()
-    """     experiment_data["experiment_name"] = experiment_record.name
+    experiment_data["experiment_name"] = experiment_record.name
     experiment_data["routine_steps"] = []
 
-    for routinestep_record in routinestep_records:
-        routinestep_data = routinestep_record_to_data(routinestep_record)
-        experiment_data["routine_steps"].append(routinestep_data)
-    """
-    return ExperimentData(experiment_data)
+    for joined_record in joined_records:
+        routinestep_record = joined_record[1]
+        object_record = joined_record[2]
+        action_record = joined_record[3]
+
+        routinestep_data = {}
+        routinestep_data["object_name"] = object_record.name
+        routinestep_data["action_name"] = action_record.name
+        (
+            routinestep_data["timing"],
+            routinestep_data["step_order"],
+        ) = timestamp_to_timing_and_order(routinestep_record.timestamp)
+
+        experiment_data["routine_steps"].append(RoutineStepData(**routinestep_data))
+
+    return ExperimentData(**experiment_data)
