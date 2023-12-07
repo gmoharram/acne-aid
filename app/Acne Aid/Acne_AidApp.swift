07b2815218534d8c1c87bcd7c498857415c4c528
Gana <gmoharram@gmail.com>
2023-12-07 16:32:55 +0100

    Frontend app

---
+++ app/Acne Aid/Acne_AidApp.swift
@@ -0,0 +1,17 @@
+//
+//  Acne_AidApp.swift
+//  Acne Aid
+//
+//  Created by Gana on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Acne_AidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
