423f40fd9e414e0d2d6811425c319b14a156616f
Gana <gmoharram@gmail.com>
2023-12-15 01:07:30 +0100

    Remove App

---
+++ app/Acne Aid/SigninView.swift
@@ -0,0 +1,18 @@
+//
+//  SigninView.swift
+//  Acne Aid
+//
+//  Created by Gana on 12/12/23.
+//
+
+import SwiftUI
+
+struct SigninView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SigninView()
+}
