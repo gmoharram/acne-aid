d645278bae8cdeb82d44ca473fce6e6219cdec7d
Gana <gmoharram@gmail.com>
2023-12-15 01:13:53 +0100

    Frontend App - Launch Screen + Signin

---
+++ app/AcneAid/AcneAidApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcneAidApp.swift
+//  AcneAid
+//
+//  Created by Gana on 12/15/23.
+//
+
+import SwiftUI
+
+@main
+struct AcneAidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SigninView()
+        }
+    }
+}
